76b6b9ca2a1a327861fc98ebdd5cf9b0a534cd90
[TAREQUE-AHMED] <[tarequeahmed46@gmail.com]>
2022-04-05 22:55:46 +0600

    update reviews site

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import CustomLink from '../CustomLink/CustomLink';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
 import './Header.css';
 
 const Header = () => {
-    const [gaping, setGaping] = useState(false);
+    const [open, setOpen] = useState(false);
     return (
         <nav className='mt-10'>
-            <div onClick={() => setGaping(!gaping)} className='bg-slate-500 md:bg-white py-3'>
-               
+            <div onClick={() => setOpen(!open)} className='bg-slate-500 md:bg-white py-3'>
+                {
+                    open ? <XIcon className='w-6 h-6 md:hidden'></XIcon> : <MenuIcon className='w-6 h-6 md:hidden'></MenuIcon>
+               }
             </div>
             <ul className='md:flex justify-center items-center gap-8 text-xl'>
                 <CustomLink to={'/home'} className='border-b-2'>HOME</CustomLink>
